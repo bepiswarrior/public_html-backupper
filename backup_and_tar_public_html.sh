cc04b632471e06472c0a7873b3338f677119c213
Samuel Satoniitty <samuelotti@users.noreply.github.com>
2017-03-30 06:01:05 +0300

    Create backup_and_tar_public_html.sh

---
+++ backup_and_tar_public_html.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Use tar to make .tar file of /var/www/ directory and name it "public_html_backup-current-date.tar"
+
+# Use scp to copy the .tar file to your local machine
+
+# Use rm to delete the .tar file from the remote machine
+
+# Print what just happened if it happened. If an error occured print a big scary error message.
+
+# That's all folks.
