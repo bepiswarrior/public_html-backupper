7c0da65af798254e9b31809e1776846dabd84f56
Samuel Satoniitty <samuelotti@users.noreply.github.com>
2017-03-30 08:06:15 +0300

    Update backup_and_tar_public_html.sh

--- backup_and_tar_public_html.sh
+++ backup_and_tar_public_html.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
-# Use tar to make .tar file of /var/www/ directory and name it "public_html_backup-current-date.tar"
-
+# For obvious reasons this script should be executed as root user.
+# Use tar to make .tar file of /var/www/ directory and name it "current-date.tar"
+cd /var/www/ && tar -czf /var/www/$(date).tar
 # Use scp to copy the .tar file to your local machine
 
 # Use rm to delete the .tar file from the remote machine
